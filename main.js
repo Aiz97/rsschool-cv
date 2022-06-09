0463e9a091e6f1e6651ffd019a58a9195f027ec6
Aizat <s.aizat21@gmail.com>
2022-06-09 19:59:30 +0600

    feat:add main page

---
+++ main.js
@@ -0,0 +1,23 @@
+const selectElement = function(element) {
+    return document.querySelector(element);
+};
+
+const menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+    body.classList.toggle('open');
+});
+
+document.addEventListener('mouseover', event => {
+    if(event.target.classList.contains('nav-link')) {
+      event.target.classList.add('active')
+    }
+  
+ });
+ 
+ document.addEventListener('mouseout', event => {
+    if(event.target.classList.contains('nav-link')) {
+      event.target.classList.remove('active')
+    }
+ });
